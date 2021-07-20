72bdee20deb3a5c0ddfa89913f6f0527db3acb8c
Osvaldo Cantone <ocantone@gmail.com>
2021-07-19 21:54:28 -0300

    V1.1 se corrigio velocidad a 9600 bps

--- src/esp32serialbluetooth.cpp
+++ src/esp32serialbluetooth.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <BluetoothSerial.h>
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+  delay(20);
 }
